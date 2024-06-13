e9e2e6f5f6885d19d0e7860b3c971b3cba85953d
Moisés <moisesespinal13@outlook.com>
2024-06-12 23:46:31 -0500

    Cambios en Repository y Service de Tecnologia

--- BCP/src/main/java/com/example/bcp/repository/TecnologiaRepository.java
+++ BCP/src/main/java/com/example/bcp/repository/TecnologiaRepository.java
@@ -2,9 +2,10 @@ package com.example.bcp.repository;
 import com.example.bcp.model.Tecnologia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
+import org.springframework.stereotype.Repository;
 import java.util.List;
 
+@Repository
 public interface TecnologiaRepository extends JpaRepository<Tecnologia, Long>{
 
     @Query(value = "SELECT id_tecnologia, nombre_tecnologia FROM public.\"Tecnologia\"",
--- BCP/src/main/java/com/example/bcp/rest/TecnologiaRest.java
+++ BCP/src/main/java/com/example/bcp/rest/TecnologiaRest.java
@@ -1,9 +1,7 @@
 package com.example.bcp.rest;
 
-import com.example.bcp.model.Tecnologia;
 import com.example.bcp.service.TecnologiaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +14,6 @@ public class TecnologiaRest {
 
     @Autowired
     private TecnologiaService tecnologiaService;
-
-    @GetMapping
-    private ResponseEntity<List<Tecnologia>> getAllTecnologias() {
-        return ResponseEntity.ok(tecnologiaService.findAll());
-    }
 
     @GetMapping("/todo")
     public List<Object[]> todoSquad() {
