2f10cfbb8dd30fc5361528608398c36700bff6ba
ErickP25 <erick.pimentel.p@uni.pe>
2024-06-19 18:43:17 -0500

    Upsi

--- BCP/src/main/java/com/example/bcp/repository/ReunionReporteConformidadRepository.java
+++ BCP/src/main/java/com/example/bcp/repository/ReunionReporteConformidadRepository.java
@@ -1,13 +1,14 @@
 package com.example.bcp.repository;
 import com.example.bcp.model.ReunionReporteConformidad;
 import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReunionReporteConformidadRepository  {
+public interface ReunionReporteConformidadRepository extends JpaRepository<ReunionReporteConformidad, Integer> {
 
     @Modifying
     @Transactional
